db3d11c2e638ee29aeb1e26e6d25147180d8605a
Luis Fernando VR <luisfvrive.98@gmail.com>
2020-05-25 22:21:22 -0500

    video 43

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
     path('', views.blog, name="blog"),
-
+    path('category/<int:category_id>/', views.category, name="category"),
 ]
