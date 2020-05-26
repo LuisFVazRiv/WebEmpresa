8eabebccdeec3f5f6e6eb065f7a567c35c75b3f6
Luis Fernando VR <luisfvrive.98@gmail.com>
2020-05-26 02:16:58 -0500

    video 48

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Page(models.Model):
     title = models.CharField(verbose_name="Título", max_length=200)
-    content = RichTextField(verbose_name="Contenido")
+    content = models.TextField(verbose_name="Contenido")
     order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
@@ -16,3 +15,4 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+ 
